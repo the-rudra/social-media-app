44bccefdfec51106a06f3686d6f2b6e43577b358
rudra <itsrudrapratapdev@gmail.com>
2023-06-20 14:05:43 +0530

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/'profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
