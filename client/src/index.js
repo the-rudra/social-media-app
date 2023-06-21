1e5d6fd8251fab336c03fe654143796a0f2c9412
rudra <itsrudrapratapdev@gmail.com>
2023-06-21 22:03:41 +0530

    login and register

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import authReducer from "./state";
-import { configureStore } from "@reduxjs/tooklit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
@@ -17,7 +17,6 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
