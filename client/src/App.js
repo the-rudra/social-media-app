08d96270aed9714808476165d06de7ed53bfd622
rudra <itsrudrapratapdev@gmail.com>
2023-06-22 20:32:32 +0530

    new url

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-import { useEffect } from "react";
 
 function App() {
   const mode = useSelector((state) => state.mode);
