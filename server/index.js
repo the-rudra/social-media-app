0e509f947c1a507a8269a83347c789f287424f8d
rudra <itsrudrapratapdev@gmail.com>
2023-07-03 21:06:38 +0530

    fixx

--- server/index.js
+++ server/index.js
@@ -53,20 +53,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-/* deployment code */
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/client/build")));
-
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-    );
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running..");
-    });
-}
-
 /* mongoose */
 const PORT = process.env.PORT || 6001;
 mongoose
